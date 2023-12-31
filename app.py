50bde0f513f488c9083a8673509191bd766546fd
arushan2 <rockarush2@gmail.com>
2023-12-31 23:35:25 +0530

    add sex input field as radio button

--- app.py
+++ app.py
@@ -2,3 +2,6 @@ import streamlit as st
 st.title("Mams Nivash")
 name = st.text_input("Enter your name:")
 age = st.number_input("Enter your age:",value=None ,min_value = 18)
+sex = st.radio(
+    "Select Your Sex",
+    ["Male", "Female", "Other"])
