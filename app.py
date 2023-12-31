06c0e643be7e9c1a6afdf57cba97b7dc7fe03e1e
arushan2 <rockarush2@gmail.com>
2023-12-31 23:24:57 +0530

    error check

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import streamlit as st
-# st.title("Mams Nivash")
-# name = st.text_input("Enter your name:")
-# age = st.number_input("Enter your age:",value=None ,min_value = 18)
+import streamlit as st
+st.title("Mams Nivash")
+name = st.text_input("Enter your name:")
+age = st.number_input("Enter your age:",value=None ,min_value = 18)
