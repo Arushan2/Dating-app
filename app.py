6e9ff0eec5e3e3bed3fb3f35ab0416e3f53dd52d
arushan2 <rockarush2@gmail.com>
2024-01-01 12:14:05 +0530

    error correction

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ age = st.number_input("Enter your age:",value=None ,min_value = 18)
 sex = st.radio(
     "Select Your Sex",
     ["Male", "Female"])
-Job_field = st.selectbox(
+job_field = st.selectbox(
     "What is  your Job fiels",
     ('Acedamic', 'IT','Realestate Business','Local business','Sales man','Manager','Medical' ))
-DOB = st.date_input("When's your birthday")
+dob = st.date_input("When's your birthday")
 user_image = st.file_uploader("Upload your image", type=["jpg", "jpeg", "png"])
 if user_image is not None:
     st.image(user_image, caption="Uploaded Image", use_column_width=True)
-Email = st.text_input("Enter your E-Mail address")
+email = st.text_input("Enter your E-Mail address")
 password = st.text_input("Create your password", type="password")
 confirm_password = st.text_input("Re-enter your password", type="password")
 
