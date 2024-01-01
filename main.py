a5fc6cbca879b2f0a60df49008d6c569af8e8907
arushan2 <rockarush2@gmail.com>
2024-01-02 00:14:47 +0530

    error check

--- main.py
+++ main.py
@@ -20,50 +20,46 @@ def save_details(file_name, expenses):
         json.dump(expenses, file)
 
 # Main Streamlit application
-def main():
-    st.title("Mams")
 
-    # Load existing expenses
-    file_name1 = "user_data.json"
-    file_name2="email_password_data.json"
-    personal_json = Details_as_json(file_name1)
-    password_json = Details_as_json(file_name2)
-    expenses1 = load_details(file_name1)
-    expenses2 = load_details(file_name2)
+st.title("Mams")
 
-
-    # Input form for new expenses
-    with st.form_submit_button("Personal details", clear_on_submit=True):
-        name = st.text_input("Enter your name:")
-        age = st.number_input("Enter your age:", value=None, min_value=18)
-        sex = st.radio("Select Your Sex", ["Male", "Female"])
-        job_field = st.selectbox("What is your Job field", ('Academic', 'IT', 'Real Estate Business', 'Local Business', 'Salesman', 'Manager', 'Medical'))
-        dob = st.date_input("When's your birthday")
-
-        # Upload user image
-        user_image = st.file_uploader("Upload your image", type=["jpg", "jpeg", "png"])
-
-        # Email and Password
-        email = st.text_input("Enter your E-Mail address")
-        password = st.text_input("Create your password", type="password")
-        confirm_password = st.text_input("Re-enter your password", type="password")
-
-        # Check if passwords match
-        passwords_match = password == confirm_password
-        if st.button("Register"):
-            if not passwords_match:
-                st.error("Passwords do not match. Please re-enter matching passwords.")
-            else:
-                expenses1.append({"name":name,"age": age , "sex":sex, "dob": str(dob), "image": user_image})
-                expenses2.append({"email":email,"password":password})
-
-    # Download expenses as JSON
-    if st.button("Download details as JSON"):
-        with open(file_name1, "r") as file:
-            st.download_button(label="Download JSON", data=file, file_name="user_data.json", mime="application/json")
-    if st.button("Download password as JSON"):
-        with open(file_name2, "r") as file:
-            st.download_button(label="Download JSON", data=file, file_name="email_password_data.json", mime="application/json")
+# Load existing expenses
+file_name1 = "user_data.json"
+file_name2="email_password_data.json"
+personal_json = Details_as_json(file_name1)
+password_json = Details_as_json(file_name2)
+expenses1 = load_details(file_name1)
+expenses2 = load_details(file_name2)
 
 
-main()
+# Input form for new expenses
+name = st.text_input("Enter your name:")
+age = st.number_input("Enter your age:", value=None, min_value=18)
+sex = st.radio("Select Your Sex", ["Male", "Female"])
+job_field = st.selectbox("What is your Job field", ('Academic', 'IT', 'Real Estate Business', 'Local Business', 'Salesman', 'Manager', 'Medical'))
+dob = st.date_input("When's your birthday")
+
+# Upload user image
+user_image = st.file_uploader("Upload your image", type=["jpg", "jpeg", "png"])
+
+# Email and Password
+email = st.text_input("Enter your E-Mail address")
+password = st.text_input("Create your password", type="password")
+confirm_password = st.text_input("Re-enter your password", type="password")
+
+# Check if passwords match
+passwords_match = password == confirm_password
+if st.button("Register"):
+    if not passwords_match:
+        st.error("Passwords do not match. Please re-enter matching passwords.")
+    else:
+        expenses1.append({"name":name,"age": age , "sex":sex, "dob": str(dob), "image": user_image})
+        expenses2.append({"email":email,"password":password})
+
+# Download expenses as JSON
+if st.button("Download details as JSON"):
+    with open(file_name1, "r") as file:
+        st.download_button(label="Download JSON", data=file, file_name="user_data.json", mime="application/json")
+if st.button("Download password as JSON"):
+    with open(file_name2, "r") as file:
+        st.download_button(label="Download JSON", data=file, file_name="email_password_data.json", mime="application/json")
