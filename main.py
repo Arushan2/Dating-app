3c897ed4d08b037a8ebc70871ff043684135f216
arushan2 <rockarush2@gmail.com>
2024-01-02 00:19:38 +0530

    error check

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ expenses2 = load_details(file_name2)
 
 # Input form for new expenses
 name = st.text_input("Enter your name:")
-age = st.number_input("Enter your age:", value=None, min_value=18)
+age = st.number_input("Enter your age:", value=None)
 sex = st.radio("Select Your Sex", ["Male", "Female"])
 job_field = st.selectbox("What is your Job field", ('Academic', 'IT', 'Real Estate Business', 'Local Business', 'Salesman', 'Manager', 'Medical'))
 dob = st.date_input("When's your birthday")
